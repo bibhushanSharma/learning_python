8d65dfbfdf8d9e2638ab34bb74bab5565a80e0a7
Bibhushan <bibhushan3@gmail.com>
2019-06-13 16:18:42 -0500

    bug fix - user input

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,16 @@
-length_of_sequense = int(input("How long do you want to sequence to be? "))
 first_number = 0
-print(first_number)
-second_number = 1
-print(second_number)
+length_of_sequense = int(input("How long do you want to sequence to be? "))
+if length_of_sequense == 1:
+    print(first_number)
+elif length_of_sequense <= 0:
+    print("This is not possible")
+else:
+    print(first_number)
+    second_number = 1
+    print(second_number)
+    for _ in range(length_of_sequense - 2):
+        current_number = first_number + second_number
+        print(current_number)
+        first_number = second_number
+        second_number = current_number
 
-for _ in range(length_of_sequense - 2):
-    current_number = first_number + second_number
-    print(current_number)
-    first_number = second_number
-    second_number = current_number
